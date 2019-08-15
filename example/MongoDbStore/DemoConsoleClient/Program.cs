f59a3f973484917af982feb5324e57ac3ff594f5
Chen Yu Pao <windperson@gmail.com>
2019-08-15 17:54:02 +0800

    add mongodb provider end-to-end test

---
+++ example/MongoDbStore/DemoConsoleClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace DemoConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console(theme: AnsiConsoleTheme.Code).CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = GetLogger<Program>(serviceProvider);
+
+            logger.LogInformation("Press space key to start demo");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    //wait
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
+
+            try
+            {
+                var demo = serviceProvider.GetService<AccessCounterDemo>();
+                await demo.RunCounter();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "error occured!");
+                throw;
+            }
+
+            logger.LogInformation("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddSerilog());
+
+            services.AddTransient<AccessCounterDemo>();
+        }
+
+        private static ILogger<T> GetLogger<T>(ServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetService<ILogger<T>>();
+        }
+    }
+}
---
+++ example/MongoDbStore/DemoSiloHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using GranDen.Orleans.NetCoreGenericHost.CommonLib;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace DemoSiloHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logConfig = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Orleans.Runtime.Management.ManagementGrain", LogEventLevel.Warning)
+                .MinimumLevel.Override("Orleans.Runtime.SiloControl", LogEventLevel.Warning)
+                .MinimumLevel.Override("Orleans.Runtime.MembershipService.MembershipTableManager", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithProcessId()
+                .Enrich.WithProcessName()
+                .Enrich.WithThreadId()
+                .Enrich.WithExceptionDetails()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .WriteTo.Trace()
+                .WriteTo.Debug();
+
+            Log.Logger = logConfig.CreateLogger();
+
+            var hostBuilder = OrleansSiloBuilderExtension.CreateHostBuilder(args).ApplySerilog();
+
+            try
+            {
+                var genericHost = hostBuilder.Build();
+                genericHost.Run();
+            }
+            catch (OperationCanceledException)
+            {
+                //do nothing
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Orleans Silo Host error");
+                throw;
+            }
+        }
+    }
+}
