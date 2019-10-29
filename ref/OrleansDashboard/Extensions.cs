514f91a3207ddcd9097ea54995ad7d46ff78fd69
Chen Yu Pao <windperson@gmail.com>
2019-10-29 12:13:17 +0800

    1. Update nugets 2. Add compatible OrleansDashbaord

---
+++ ref/OrleansDashboard/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace OrleansDashboard
+{
+    internal static class Extensions
+    {
+        internal static string ToValue(this PathString path)
+        {
+            return WebUtility.UrlDecode(path.ToString().Substring(1));
+        }
+    }
+}
